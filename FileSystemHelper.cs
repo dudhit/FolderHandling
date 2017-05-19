84a4af693a178561493f76c20a7569bbd880f0e3
dudhit <dudhit@users.noreply.github.com>
2017-05-19 15:25:44 +0930

    Exception handling

--- FileSystemHelper.cs
+++ FileSystemHelper.cs
@@ -8,7 +8,7 @@ using System.Windows;
 
 namespace SoloProjects.Dudhit.Utilities
 {
- public class FileSystemHelper
+  public static class FileSystemHelper
   {
     public static bool FolderCreation(string path)
     {
@@ -17,45 +17,114 @@ namespace SoloProjects.Dudhit.Utilities
         if(!Directory.Exists(path))
         {
           Directory.CreateDirectory(path);
-          return true;
+          return FolderVerification(path);
         }
       }
       catch(UnauthorizedAccessException UAE)
       {
-        MessageBox.Show(UAE.Message, "info", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
-        return false;
+        throw new ApplicationException("Failure to create folder using FileSystemHelper", UAE);
+      }
+      catch(DirectoryNotFoundException DNFE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", DNFE);
+      }
+      catch(PathTooLongException PTLE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", PTLE);
       }
       return false;
+
     }
 
     public static bool FolderVerification(string path)
     {
       try
       {
-        if(Directory.Exists(path))
-        {
-          return true;
-        }
+        return Directory.Exists(path);
       }
       catch(UnauthorizedAccessException UAE)
       {
-        MessageBox.Show(UAE.Message, "info", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
-        return false;
+        throw new ApplicationException("Failure to create folder using FileSystemHelper", UAE);
       }
-      return false;
+      catch(DirectoryNotFoundException DNFE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", DNFE);
+      }
+      catch(PathTooLongException PTLE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", PTLE);
+      }
+
     }
+
     public static bool CopyFile(string sourcePath, string destinationPath)
     {
       try
       {
-        File.Copy(sourcePath, destinationPath,true);
+        File.Copy(sourcePath, destinationPath, true);
         return true;
       }
       catch(UnauthorizedAccessException UAE)
       {
-        MessageBox.Show(UAE.Message, "info", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
-        return false;
+        throw new ApplicationException("Failure to create folder using FileSystemHelper", UAE);
       }
+      catch(DirectoryNotFoundException DNFE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", DNFE);
       }
+      catch(PathTooLongException PTLE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", PTLE);
+      }
+    }
+
+    public static bool FileExists(string path)
+    {
+      try
+      {
+        if(File.Exists(path))
+          return true;
+      }
+      catch(UnauthorizedAccessException UAE)
+      {
+        throw new ApplicationException("Failure to create folder using FileSystemHelper", UAE);
+      }
+      catch(FileNotFoundException FNFE)
+      {
+        throw new FileNotFoundException("Failure to locate folder using FileSystemHelper", FNFE);
+      }
+      catch(PathTooLongException PTLE)
+      {
+        throw new DirectoryNotFoundException("Failure to locate folder using FileSystemHelper", PTLE);
+      }
+      return false;
+    }
+
+    public static void Logging(string path, string line)
+    {
+      try
+      {
+        using(StreamWriter sw = new StreamWriter(path, true))
+        {
+          sw.Write(DateTime.Now.ToString("yyyy-MM-dd"));
+          sw.Write(" ");
+          sw.Write(DateTime.Now.ToString("HH:mm:ss"));
+          sw.Write(" ");
+          sw.WriteLine(line);
+        }
+      }
+      catch(UnauthorizedAccessException UAE)
+      {
+        throw new ApplicationException("Failure to write to log FileSystemHelper", UAE);
+      }
+      catch(FileNotFoundException FNFE)
+      {
+        throw new FileNotFoundException("Failure to write to log using FileSystemHelper", FNFE);
+      }
+      catch(PathTooLongException PTLE)
+      {
+        throw new DirectoryNotFoundException("Failure to write to log using FileSystemHelper", PTLE);
+      }
+    }
   }
 }
